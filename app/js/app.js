2118d8d6492c53cf06e529d40b38d8270b37ee2e
Marish04 <94753536+Marish04@users.noreply.github.com>
2022-02-11 13:37:35 -0800

    Add header

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,40 @@
-$(document).ready(function () {});
+// $(document).ready(function () {
+//     $('.burger').click(function(event) {
+//         $('.burger,.menu').toggleClass('active');
+//     });
+// });
+
+const menuBtn = document.querySelector(".burger");
+const menu = document.querySelector(".menu");
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+    if(!menuOpen) {
+        menuBtn.classList.add("open");
+        menu.classList.add("open");
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove("open");
+        menu.classList.remove("open");
+        menuOpen = false;
+    }
+});
+
+// const menuBtn = document.querySelector(".burger");
+// const menu = document.querySelector(".menu");
+// let menuOpen = false;
+// menuBtn.addEventListener("click", () => {
+//     if(!menuOpen) {
+//         menu.classList.add("open");
+//         menuOpen = true;
+//     } else {
+//         menu.classList.remove("open");
+//         menuOpen = false;
+//     }
+// });
+
+
+
+
+
+
+
